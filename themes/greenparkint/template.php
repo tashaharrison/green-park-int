71edc41975d846e4841f7876eeaa7e8584ef1d07
Tasha Harrison <pete@spirelightmedia.com>
2014-08-20 18:05:17 +0200

    responsive menu

--- themes/greenparkint/template.php
+++ themes/greenparkint/template.php
@@ -5,3 +5,7 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * greenparkint theme.
  */
+ 
+drupal_add_js (drupal_get_path('theme','greenparkint') .'/js/script.js');
+
+?>
